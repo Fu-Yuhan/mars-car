784d844a160aabf241ccbc73a6a16de555d696f1
Fu Yuhan <152140410+Fu-Yuhan@users.noreply.github.com>
2025-05-01 17:03:55 +0800

    feat(main): 添加路口识别和转向逻辑
    - 在主循环中增加了对不同路口的识别逻辑
    - 添加了在不同情况下进行左转或右转的操作
    - 引入了状态变量来跟踪车辆的不同行驶阶段
    - 优化了代码结构，增加了注释以提高可读性

--- main.py
+++ main.py
@@ -6,18 +6,21 @@ import math
 true=True
 def main():
     # 任务1 巡线
-    turn_time = 3
+    turn_time = 3# 转弯时间
+    status = "not started"
     # 初始化底盘
     car = LineFollowMgr()
+    turn_side = None
     car.reset()
     car.start()
+    status = "before detected"
     # 初始化摄像头
     camera = Camera.Camera()
     camera.camera_open(correction=True)
 
     while True:
         # TODO:检测路标
-
+        # TODO: status = "after detected"
         # 检测前方路况
         sonar = car.line.readData()
         if sonar[0] & sonar[1] & sonar[2] & sonar[3]:# 遇到转弯1
@@ -25,6 +28,27 @@ def main():
             car.car.set_velocity(math.pi*(car.car.a**2)/(4*turn_time),90,math.pi/(2*turn_time))# TODO:根据识别的结果左转或右转
             time.sleep(turn_time)
             car.start()
+            status = "after first cross"
+
+        # TODO: status = "after carried rocks"
+        # TODO: status = "after lunched rocket"
+
+        if sonar[0] and sonar[1] and sonar[2] and not sonar[3]:
+            car.stop()
+            car.car.set_velocity(math.pi * (car.car.a ** 2) / (4 * turn_time), 90,
+                                 math.pi / (2 * turn_time))  # 左转
+            time.sleep(turn_time)
+            car.start()
+            status = "after 2nd cross"
+
+        if not sonar[0] and sonar[1] and sonar[2] and sonar[3]:
+            car.stop()
+            car.car.set_velocity(math.pi * (car.car.a ** 2) / (4 * turn_time), 90,
+                                 -math.pi / (2 * turn_time))  # 左转
+            time.sleep(turn_time)
+            car.start()
+            status = "after 2nd cross"
+
 
 
 
